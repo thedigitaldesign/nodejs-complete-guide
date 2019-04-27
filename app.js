06455e9d1f22653af55eef1f2b6dc8fed867ce91
OneJChristensen <joshua@thedigitaldesign.com>
2019-04-26 22:20:39 -0600

    Assignment 2 was pretty straight forward. Not gotchas.

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require('http')
-
 // Packages
 const express = require('express')
 
@@ -10,19 +8,24 @@ const express = require('express')
 const app = express()
 
 //-- NOTE:  .use will be used for every incoming request
-app.use((request, response, next) => {
-    console.log('In the middleware')
-    next() //-- NOTE: Allows the request to continue to the next middleware in-line
+// app.use((request, response, next) => {
+//     console.log('In the middleware')
+//     next() //-- NOTE: Allows the request to continue to the next middleware in-line
+// })
+
+app.use('/add-product', (request, response, next) => {
+    response.send(`
+        <h1>Add Product!</h1>
+    `)
+    //-- NOTE:  Do not call next() after you send, it will execute the next .use(...)
+    //--        which is not desired functionality and result in an error.
 })
 
-app.use((request, response, next) => {
+app.use('/', (request, response, next) => {
     console.log('In another middleware')
-
     response.send(`
         <h1>Hello from Express.js</h1>
     `)
-
-    //response.end()
 })
 
 // const server = http.createServer(app)
---
+++ assignments/02_express-assignment/app.js
@@ -0,0 +1,37 @@
+// Packages
+const express = require('express')
+
+// Exports
+
+// App
+const app = express()
+let html = ``
+
+//-- Task 2
+// app.use((request, response, next) => {
+//     console.log('I am in the first app.use(...)')
+//     next()
+// })
+
+// app.use((request, response, next) => {
+//     console.log('I am in the SECOND app.use(...)')
+//     response.end()
+// })
+
+//-- Task 3
+app.use('/users', (request, response, next) => {
+    html = `
+        <h1>Users</h1>
+    `
+    response.send(html)
+})
+
+app.use((request, response, next) => {
+    html = `
+        <h1>Home</h1>
+    `
+    response.send(html)
+})
+
+// Server
+app.listen(3200)
